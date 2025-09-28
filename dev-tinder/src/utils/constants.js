1b52674e02b51b0636f2ff6ef188ac9270deac32
rohitramchandanistm1996@gmail.com <Radha@1234567890->
2025-09-28 22:12:53 +0530

    Made the Base_URL Dynamic

--- dev-tinder/src/utils/constants.js
+++ dev-tinder/src/utils/constants.js
@@ -1,5 +1,5 @@
 /* It is for Local Testing */
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = location.hostname === "localhost" ? "http://localhost:3000" : "/api";
 
 /* It is for Deployment */
 // export const BASE_URL = "/api";
