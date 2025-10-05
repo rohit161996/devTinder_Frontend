39991832c07e2e9d60d3b4c15c824b950c63f266
rohitramchandanistm1996@gmail.com <Radha@1234567890->
2025-10-06 02:07:31 +0530

    Live Messages fetched from the database on page rendering and few css modification done.

--- dev-tinder/src/Components/Chat.jsx
+++ dev-tinder/src/Components/Chat.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { createSocketConnection } from '../utils/socket';
 import { useSelector } from 'react-redux';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
 
 const Chat = () => {
 
@@ -15,12 +17,37 @@ const Chat = () => {
         const socket = createSocketConnection();
         socket.emit("sendMessage", {
             firstName: user.firstName,
+            lastName: user.lastName,
             userId,
             targetUserId,
             text: newMessage
         });
         setNewMessage("");
     }
+
+    const fetchChatMessages = async () => {
+        const chat = await axios.get(
+            BASE_URL + "/chat/" + targetUserId,
+            { withCredentials: true }
+        );
+
+        /* We get the messages object from the chat */
+        console.log(chat.data.messages);
+
+        const chatMessages = chat?.data?.messages.map((msg) => {
+            return {
+                firstName: msg?.firstName,
+                lastName: msg?.lastName,
+                text: msg?.text
+            };
+        });
+
+        setMessages(chatMessages);
+    }
+
+    useEffect(() => {
+        fetchChatMessages();
+    }, []);
 
     useEffect(() => {
         if (!userId) {
@@ -42,9 +69,9 @@ const Chat = () => {
 
         socket.on(
             "messageReceived",
-            ({ firstName, text }) => {
-                console.log(firstName + " : " + text);
-                setMessages((messages) => [...messages, { firstName, text }]);
+            ({ firstName, lastName, text }) => {
+                console.log(firstName + " " + lastName + " : " + text);
+                setMessages((messages) => [...messages, { firstName, lastName, text }]);
             }
         )
 
@@ -55,7 +82,7 @@ const Chat = () => {
     }, [userId, targetUserId]);
 
     return (
-        <div className="border border-gray-600 w-3/4 mx-auto m-5 h-[70vh] flex flex-col">
+        <div className="border border-gray-600 w-3/4 mx-auto m-5 h-[70vh] flex flex-col bg-gray-900">
             {/* Heading with Chat written in it. */}
             <h1 className="p-5 border-5 border-gray-600">
                 Chat
@@ -66,15 +93,26 @@ const Chat = () => {
                 {/* Display Messages */}
                 {
                     messages.map((msg, index) => {
-                        return <div key={index} className="chat chat-start">
+                        return <div
+                            key={index}
+                            className="chat chat-start text-white">
                             <div className="chat-header">
-                                {msg.firstName}
-                                <time className="text-xs opacity-50">2 hours ago</time>
+                                {/* FirstName LastName*/}
+                                {
+                                    `${msg.firstName} ${msg.lastName}`
+                                }
+
+                                {/* Timestamp */}
+                                <time className="text-xs opacity-50">
+                                    2 hours ago
+                                </time>
                             </div>
                             <div className="chat-bubble">
                                 {msg.text}
                             </div>
-                            <div className="chat-footer opacity-50">Seen</div>
+                            <div className="chat-footer opacity-50">
+                                Seen
+                            </div>
                         </div>
                     })
                 }
